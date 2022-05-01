4baaf2c5e403fa2295343457d5b9116c74d0a111
Sai Sharma <89261592+saisharma9@users.noreply.github.com>
2022-05-01 13:23:49 -0500

    Update HomeViewController.swift

--- SaraSchedules/HomeViewController.swift
+++ SaraSchedules/HomeViewController.swift
@@ -7,38 +7,38 @@
 
 import UIKit
  
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todo.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = taskTableViewOutlet.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
-        cell.textLabel?.text = todo[indexPath.row][0]
-        return cell
-    }
-
-    @IBOutlet weak var taskTableViewOutlet: UITableView!
+class HomeViewController: UIViewController {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return todo.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        var cell = taskTableViewOutlet.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+//        cell.textLabel?.text = todo[indexPath.row][0]
+//        return cell
+//    }
+//
+//    @IBOutlet weak var taskTableViewOutlet: UITableView!
         
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        taskTableViewOutlet.delegate = self
-        taskTableViewOutlet.dataSource = self
+//        taskTableViewOutlet.delegate = self
+//        taskTableViewOutlet.dataSource = self
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        taskTableViewOutlet.reloadData()
-    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        taskTableViewOutlet.reloadData()
+//    }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            var transition = segue.identifier
-            if transition == "taskInfoSegue"{
-                var destination = segue.destination as! InfoViewController
-                
-            }
-    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//            var transition = segue.identifier
+//            if transition == "taskInfoSegue"{
+//                var destination = segue.destination as! InfoViewController
+//
+//            }
+//    }
 
-    @IBAction func addButtonClicked(_ sender: UIBarButtonItem) {
-    }
+//    @IBAction func addButtonClicked(_ sender: UIBarButtonItem) {
+//    }
 }
