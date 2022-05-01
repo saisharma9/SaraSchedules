fa7a0da215fc5cb463af02b0fb870f554359347e
Sai Sharma <89261592+saisharma9@users.noreply.github.com>
2022-05-01 13:23:44 -0500

    Update AddViewController.swift

--- SaraSchedules/AddViewController.swift
+++ SaraSchedules/AddViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
 class AddViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
@@ -14,19 +16,43 @@ class AddViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func unwindToDashBoard(){
+        performSegue(withIdentifier: "backToHome", sender: self)
     }
-    */
+    
+    func messageAlert(title:String, message:String) {
+            let errorAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+            
+            errorAlert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: { (ACTION) in
+                errorAlert.dismiss(animated: true, completion: nil)
+            }))
+            self.present(errorAlert, animated: true, completion: nil)
+        }
+    
+    func navigateAlert(title:String, message:String) {
+            let errorAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+            
+            errorAlert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: { (ACTION) in
+                
+                //self.unwindToDashBoard()
+            }))
+            self.present(errorAlert, animated: true, completion: nil)
+        }
 
+    func uploadToFirebase(){
+        let taskTitle = titleTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let taskDescription = reminderTextView.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let db = Firestore.firestore()
+        db.collection("RemindersDB").addDocument(data: ["Title" : taskTitle,
+                                                        "Task" : taskDescription
+                                                       ]) { (error) in
+            if error != nil {
+                self.messageAlert(title: "Data fetch Error", message: "We have experienced an error while fetching your data. Please try again.")
+            }
+            self.navigateAlert(title: "Success", message: "Uploaded to Database successfully")
+        }
+    }
 }
