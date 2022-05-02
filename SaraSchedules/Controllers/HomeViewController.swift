9163853bd2566d49b826df0c603f8bfe7560a044
Sai Sharma <89261592+saisharma9@users.noreply.github.com>
2022-05-02 18:41:59 -0500

    Create HomeViewController.swift

---
+++ SaraSchedules/Controllers/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SaraSchedules
+//
+//  Created by student on 4/7/22.
+//
+
+import UIKit
+ 
+class HomeViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
