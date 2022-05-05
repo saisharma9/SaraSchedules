1545acff59d3f610d268d019f6e607a095d650ed
akshaykrishna129 <89559305+akshaykrishna129@users.noreply.github.com>
2022-05-04 20:20:09 -0500

    Update Tasks.swift

--- SaraSchedules/Helpers/Tasks.swift
+++ SaraSchedules/Helpers/Tasks.swift
@@ -9,7 +9,14 @@ import Foundation
 import UIKit
 
 class Tasks {
-    var Title: String?
-    var Description: String?
+    var id: String
+    var Title: String
+    var Description: String
+    
+    init(id: String, Title: String, Description: String) {
+        self.id = id
+        self.Title = Title
+        self.Description = Description
+    }
 }
 
