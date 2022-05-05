09653d7c0dfbae7bb5934ab73d40bd8141e35080
akshaykrishna129 <89559305+akshaykrishna129@users.noreply.github.com>
2022-05-04 20:20:06 -0500

    Update ListViewController.swift

--- SaraSchedules/Controllers/ListViewController.swift
+++ SaraSchedules/Controllers/ListViewController.swift
@@ -8,59 +8,65 @@
 import UIKit
 import Firebase
 
-class ListViewController: UIViewController {
-    
-    //@IBOutlet var tasksTableView: UITableView!
+class ListViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+        
+    var tasks = [Tasks]()
+    var dbReadReference: DatabaseReference?
+    @IBOutlet weak var tasksTableView: UITableView!{
+        didSet{
+            tasksTableView.dataSource = self
+            tasksTableView.delegate = self
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        tasksTableView.dataSource = self
-//        tasksTableView.delegate = self
-//        tasksTableView.reloadData()
-//        let dbReadReference = Database.database().reference()
-//        self.tasks.removeAll()
-//        dbReadReference.child("Tasks").observe(.value) { (snapshot) in
-//            if snapshot.value != nil{
-//                for child in snapshot.children {
-//                    let task = Tasks()
-//                    let childSnapshot = child as! DataSnapshot
-//                    if childSnapshot.hasChild("Title") {
-//                        task.Title = childSnapshot.childSnapshot(forPath: "Title").value as! String
-//                    }
-//                    if childSnapshot.hasChild("Description") {
-//                        task.Description = childSnapshot.childSnapshot(forPath: "Description").value as! String
-//                    }
-//                    self.tasks.append(task)
-//                }
-//            }
-//        }
+        fetchDataFromDatabase()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let task = tasks[indexPath.row]
+        let cell = tasksTableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        cell.textLabel?.text = task.Title
+        return cell
+    }
+    
+    func fetchDataFromDatabase(){
+        dbReadReference = Database.database().reference().child("Tasks")
+        dbReadReference?.observe(.childAdded){ [weak self](snapshot) in
+            let key = snapshot.key
+            guard let value = snapshot.value as? [String : Any] else {return}
+            if let title = value["Title"] as? String, let description = value["Description"] as? String {
+                let task = Tasks(id: key, Title: title, Description: description)
+                self?.tasks.append(task)
+                if let row = self?.tasks.count {
+                    let indexPath = IndexPath(row: row-1, section: 0)
+                    self?.tasksTableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
     }
     
 
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let indexPath = self.tasksTableView.indexPathForSelectedRow
+        let id = segue.identifier
+        if id == "displayTask"{
+            let destination = segue.destination as! InfoViewController
+            destination.reminderTextView.text = tasks[indexPath!.row].Description
+            print(tasks[indexPath!.row].Description)
+        }
     }
-    */
 }
 
-//extension ListViewController: UITableViewDelegate, UITableViewDataSource {
-//
-////    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-////        self.tasks.count
-////    }
-////
-////    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-////        let cell = tasksTableView.dequeueReusableCell(withIdentifier: "taskTitle")
-////        let task = self.tasks[indexPath.row]
-////        cell?.textLabel!.text = task.Title
-////        return cell!
-////    }
-//
-//
-//}
+
+
